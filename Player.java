43c6f78da3698cefe3351d262c4f090b1f84ba14
abanda2 <80859595+abanda2@users.noreply.github.com>
2021-03-21 17:12:10 -0400

    Update Player.java

--- Player.java
+++ Player.java
@@ -39,4 +39,22 @@ public class Player {
 		this.locationID = locationID;
 	}
 	
+	 public void testDBConnection() {
+    		String username = "ADMIN";
+    		String password = "TeamYeti2021";          
+    		String server = "jdbc:oracle:thin:@adb.us-ashburn-1.oraclecloud.com:1522:yrmks4zvyivxii7_db202103201405_low.adb.oraclecloud.com";  
+    		java.security.Security.setProperty("networkaddress.cache.ttl" , "0");
+
+    		// With AutoCloseable, the connection is closed automatically.
+    		try {
+      			Class.forName("oracle.jdbc.driver.OracleDriver");
+      			Connection con = DriverManager.getConnection(server, username, password);
+      			System.out.print("Connection Successful");
+    		}   
+    		catch (SQLException e)
+   			 {System.out.print(e);}
+   		catch (ClassNotFoundException e2)
+    			{System.out.print(e2);}
+ 	}
+	
 }
