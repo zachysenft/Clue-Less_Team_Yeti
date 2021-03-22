b9c8e6eb3d5dabce2ab0e9123b663e531bb3b483
Zach Senft <63065752+zachysenft@users.noreply.github.com>
2021-03-22 14:49:42 -0400

    Getter methods and Suggestion List in Player class

--- Player.java
+++ Player.java
@@ -1,7 +1,15 @@
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.*;
+
+
+import com.sun.jdi.connect.spi.Connection;
+
+
 /*
  * Player Class for Team Yeti's Clue-Less game.
  * 
- * Date: 3/18/2021
+ * Date: 3/22/2021
  */
 
 
@@ -14,6 +22,7 @@ public class Player {
 	boolean activeFlg;
 	int orderNbr;
 	Locations locationID;
+	Suggestions[] suggestionList;
 	
 		
 	//Constructors initialization
@@ -21,8 +30,16 @@ public class Player {
 		this.playerID = playerID;
 	}
 	
+	public int getPlayerID(Player player) {
+		return player.playerID;
+	}
+	
 	public void setPlayerNm(String playerNm) {
 		this.playerNm = playerNm;
+	}
+	
+	public String getPlayerNm(Player player) {
+		return player.playerNm;
 	}
 	
 	//This constructor should be used to set the boolean value of activeFlg to true when called and passed a created playerID
@@ -35,20 +52,38 @@ public class Player {
 		this.orderNbr = orderNbr;
 	}
 	
+	public int getOrderNbr(Player player) {
+		return player.orderNbr;
+	}
+	
 	public void setLocation(Locations locationID) {
 		this.locationID = locationID;
 	}
+	
+	public Locations getLocation(Player player) {
+		return player.locationID;
+	}
+	
+	//Array of Suggestions the player has made
+	public void suggestionMade(Player player, Suggestions suggestion) {
+		
+		suggestion.append(suggestion, player.suggestionList);
+		
+	}
+	
+	
+	
 	
 	 public void testDBConnection() {
     		String username = "ADMIN";
     		String password = "TeamYeti2021";          
     		String server = "jdbc:oracle:thin:@adb.us-ashburn-1.oraclecloud.com:1522:yrmks4zvyivxii7_db202103201405_low.adb.oraclecloud.com";  
-    		java.security.Security.setProperty("networkaddress.cache.ttl" , "0");
+    		//java.security.Security.setProperty("networkaddress.cache.ttl" , "0");
 
     		// With AutoCloseable, the connection is closed automatically.
     		try {
       			Class.forName("oracle.jdbc.driver.OracleDriver");
-      			Connection con = DriverManager.getConnection(server, username, password);
+      			Connection con = (Connection) DriverManager.getConnection(server, username, password);
       			System.out.print("Connection Successful");
     		}   
     		catch (SQLException e)
