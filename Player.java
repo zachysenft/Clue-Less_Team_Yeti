64e4f61621614048a29b372d519d43d0cd3a7fef
zachysenft <senftzach11@gmail.com>
2021-04-04 19:22:47 -0400

    Easy methods done

--- Player.java
+++ Player.java
@@ -1,49 +1,75 @@
 package clueLess;
-
-
 import clueLess.Locations.LocationType;
 import yeti_client_server.YetiClient;
+import yeti_client.PlayersClientService;
 
-//import yeti_client.PlayersClientService;
+/*
+ * Zach update 4/4/21
+ */
 
 public class Player {
 	//Creating attributes
-		int playerID;
-		String playerNm;
-		boolean activeFlg;
-		int orderNbr;
-		private LocationType locationID;
+	
+	int playerID;
+	String playerName;
+	boolean activeFlag;
+	boolean LostGameFlag;
+	int orderNum;
+	private Location location;
+	private Card playerCard[];
+	
+	
+	public void setPlayerID(int playerID) {
+		this.playerID = playerID;
+	}
+	public int getPlayerID() {
+		return this.playerID;
+	}
+	public void setPlayerName(String playerName) {
+		this.playerName = playerName;
+	}
+	public String getPlayerName() {
+		return this.playerName;
+	}
+	
+	public void setPlayerActive() {
+		this.activeFlag = true;
+		System.out.println(this.playerName + " with ID: " + this.playerID + " has been activated.");
+	}
+	
+	public void setOrderNum(int orderNum) {
+		this.orderNum = orderNum;
+	}
+	
+	public int getOrderNum() {
+		return this.orderNum;
+	}
+	
+	public void setLocation(Location location) {
+		this.location = location;
+	}
+	
+	public Location getLocation() {
+		return this.location;
+	}
+	
+	public Player printPlayer() {
+		return "Player: " + this.playerName + "\nID: " + this.playerID + "\nOrder Number: " + this.orderNum;
+	}
+	
+	public void setLostGameFlag() {
+		this.LostGameFlag = true;
+		System.out.println(this.playerName + " has lost the game.");
+	}
+	
+	public boolean getLostGameFlag() {
+		return this.LostGameFlag;
+	}
+	
+	public void sendMessage() {
+		YetiClient msg = new YetiClient();
+		msg.broadcastAccusation(this);
 		
-			
-		//Constructors initialization
-		public void setPlayerID(int playerID) {
-			this.playerID = playerID;
-		}
-		public int getPlayerID() {
-			return this.playerID;
-		}
-		public void setPlayerNm(String playerNm) {
-			this.playerNm = playerNm;
-		}
-		
-		//This constructor should be used to set the boolean value of activeFlg to true when called and passed a created playerID
-		public String activatePlayer(int playerID) {
-			this.activeFlg = true;
-			return "Player: " + playerID + " has been activated.";
-		}
-		
-		public void setOrderNbr(int orderNbr) {
-			this.orderNbr = orderNbr;
-		}
-		
-		public void setLocation(Locations locationID) {
-			this.locationID = locationID.getLocation();
-		}
-		
-		public void subscribeMsg() {
-			YetiClient msg = new YetiClient();
-			msg.broadcastAccusation(this);
-			
-		}
+	}
 
 }
