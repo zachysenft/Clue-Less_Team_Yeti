8ebc147504b6878264b066e734071977e12d9814
zachysenft <senftzach11@gmail.com>
2021-04-08 18:57:33 -0400

    Framework of Move()

--- src/Player.java
+++ src/Player.java
@@ -99,23 +99,91 @@ public class Player {
 		
 	}
 
-	//Zach... I developed a schematic of numbering for locations that will work for this algo
+	//Matt... I developed a schematic of numbering for locations that will work for this algo
 	//Returns where the player location should update to, then another method will update it.
+	//include an if in driver method to retry if returned is 0.
+	//Need to add in conditionals of hallways being occupied
 	
 	public int move(int curLoc, int destLoc) {
 		
 		if(curLoc == 1) {
-			
 			//In room 1, need destLoc to be 12 or 14 hallway...
-			//Ensure that input is okay, then update and break
-			if(destLoc > 15) {
-				System.out.println("Invalid Input");
-				//retry to call method
+			if(destLoc == 12 || destLoc == 14) {
+				return destLoc;
 			}
-			
-			return destLoc;
-			
+			return 0;
 		}
+		
+		if(curLoc == 2) {
+			//In room 2, need destLoc to be 12, 25, or 23 hallway
+			if(destLoc == 12 || destLoc == 25 || destLoc == 23) {
+				return destLoc;
+			}
+			return 0;
+		}
+		
+		if(curLoc == 3) {
+			//In room 3, need destLoc to be 23 or 36
+			if(destLoc == 23 || destLoc == 36) {
+				return destLoc;
+			}
+			return 0;
+		}
+		
+		if(curLoc == 4) {
+			//In room 4, need destLoc 14, 47, 45
+			if(destLoc == 14 || destLoc == 45 || destLoc == 47) {
+				return destLoc;
+			}
+			return 0;
+		}
+		
+		if(curLoc == 5) {
+			//In room 5, need destLoc 45, 58, 56, 25
+			if(destLoc == 45 || destLoc == 58 || destLoc == 56 || destLoc == 25) {
+				return destLoc;
+			}
+			return 0;
+		}
+		
+		if(curLoc == 6) {
+			//In room 6, need destLoc 36, 56, 69
+			if(destLoc == 36 || destLoc == 56 || destLoc == 69) {
+				return destLoc;
+			}
+			return 0;
+		}
+		
+		if(curLoc == 7) {
+			//In room 7, need destLoc 47, 78
+			if(destLoc == 47 || destLoc == 78) {
+				return destLoc;
+			}
+			return 0;
+		}
+		
+		if(curLoc == 8) {
+			//In room 8, need destLoc 78, 58, 89
+			if(destLoc == 78 || destLoc == 58 || destLoc == 89) {
+				return destLoc;
+			}
+			return 0;
+		}
+		
+		if(curLoc == 9) {
+			//In room 9, need destLoc 69 or 89
+			if(destLoc == 69 || destLoc == 89) {
+				return destLoc;
+			}
+			return 0;
+		}
+		
+		//All rooms done, need to do all twelve hallways now.
+		//Need to add in if a hallway is preoccupied by checking other player's locationID's
+		
+		
+		
+		
 		
 		
 		
