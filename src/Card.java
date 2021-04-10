df2aa68c9461bd153f564ebeeef03f524f37f503
Zach Senft <63065752+zachysenft@users.noreply.github.com>
2021-04-10 15:02:03 -0400

    Add files via upload

--- src/Card.java
+++ src/Card.java
@@ -5,7 +5,7 @@ public class Card{
 	String type;
 	String name;
 	
-	public Card(String type, String name) {
+	public Card(String name, String type) {
 		this.type = type;
 		this.name = name;
 	}
@@ -22,6 +22,35 @@ public class Card{
 		System.out.println("Name: "+ this.name);
 		System.out.println("Type: "+ this.type);
 	}
+	
+	public enum cardLists {
+		MUSTARD("Colonel Mustard", "person"), WHITE("Mrs. White", "person"), PLUM(
+				"Professor Plum", "person"), PEACOCK("Mrs. Peacock", "person"), GREEN(
+				"Mr. Green", "person"), SCARLET("Miss Scarlet", "person"), DAGGER(
+				"Dagger", "weapon"), ROPE("Rope", "weapon"), PIPE("Lead Pipe",
+				"weapon"), CANDLESTICK("Candlestick", "weapon"), REVOLVER(
+				"Revolver", "weapon"), WRENCH("Wrench", "weapon"), KITCHEN(
+				"Kitchen", "room"), BALLROOM("Ballroom", "room"), DININGROOM(
+				"Dining Room", "room"), LOUNGE("Lounge", "room"), HALL("Hall",
+				"room"), CONSERVATORY("Conservatory", "room"), BILLIARD(
+				"Billiard Room", "room"), LIBRARY("Library", "room"), STUDY(
+				"Study", "room");
+		
+		private String cardName;
+		private String cardType;
+		
+		private cardLists(String cardName, String cardValue) {
+			this.cardName = cardName;
+			this.cardType = cardValue;
+		}
+		
+		public cardLists[] createCard() {
+			return cardLists.values();
+		}
+		
+		
+	}
+
 
 	
 }
